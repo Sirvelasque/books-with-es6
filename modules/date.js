6a9a8a774ccf08caf2c6b149f820af21e56a25ed
Sirvelasque <velasquezwalter10@yahoo.es>
2022-02-25 09:25:01 -0600

    Update date.js

--- modules/date.js
+++ modules/date.js
@@ -1,4 +1,4 @@
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import { DateTime } from './../node_modules/luxon/src/luxon.js';
 
 const showdate = () => {
   const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
